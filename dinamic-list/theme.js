3fd632b8a11ddf84807a648092477d6959c56719
oseto <omar.trafalgar.2001@gmail.com>
2026-03-23 03:16:15 -0500

    mejora estilos dark mode de dinamic-list

---
+++ dinamic-list/theme.js
@@ -0,0 +1,31 @@
+const themeIcon = document.getElementById('iconTheme')
+const themeModeInput = document.getElementById('inputTheme')
+const html = document.querySelector('html')
+
+console.log(themeModeInput)
+
+themeModeInput.addEventListener('change',(e)=>{
+    if(e.target.checked){
+        setThemeToDark()
+    }else{
+        setThemeToNormal()
+    }
+})
+
+if (localStorage.getItem('theme') == 'dark'){
+    themeModeInput.checked = true
+    setThemeToDark()
+}
+
+// funciones
+function setThemeToDark(){
+    localStorage.setItem('theme','dark')
+    html.classList.add('dark');
+    themeIcon.className = 'bi bi-brightness-high-fill'
+}
+
+function setThemeToNormal(){
+    localStorage.setItem('theme','normal')
+    html.classList.remove('dark')
+    themeIcon.className = 'bi bi-moon-fill'
+}
